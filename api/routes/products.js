436a114fc074423beec5abf41ace671181bfef86
Garo San <garosanchez27@gmail.com>
2019-03-02 20:37:51 -0600

    Adding orders.js, products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,2 +1,43 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /products"
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "Handling POST requests to /products",
+    id
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "You discovered the special ID"
+    });
+  } else {
+    res.status(200).json({
+      message: "You passed an id",
+      id
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated product"
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Deleted product"
+  });
+});
+
+module.exports = router;
